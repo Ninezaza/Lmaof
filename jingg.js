f69569138be63081b72fcd123f9fe5d4de9ed115
Ninezaza <123223339+Ninezaza@users.noreply.github.com>
2023-01-21 11:24:13 +0700

    add js and css

---
+++ jingg.js
@@ -0,0 +1,66 @@
+let n = prompt("ชื่ออะไรหรออ")
+console.log(n)
+alert("สวัสดีครับเจ๊" + n)
+var h = "halo halo "
+var g = " >/////<"
+console.log(h)
+console.log(g)
+document.getElementById("r").innerHTML=h+n+g
+let b = prompt("ปกติชอบส่งแล้วรีบยกเลิกข้อความใช่มั้ยย ถ้าใช่กด 1 ถ้าไม่กด 2")
+if(b==1)
+{document.getElementById ("content").innerHTML =
+"<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/6bGdcg1OZOQ\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
+alert("งอนแล้วนะๆๆๆแต่มีไรให้ดูด้วยแหละะ")
+
+}
+else if(b==2)
+{document.getElementById ("content").innerHTML = 
+"<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/0Os906JoKy0\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
+alert("จริงดิน้ำตาจะไหลแล้วว"+n)
+}
+else
+{
+console.log(n)
+    alert("กวนนะ"+n+"กวนteenจริงๆ")
+}
+function n1()
+{
+    document.getElementById("demo").innerHTML = s =prompt("ชอบดองแชทป่าวว ถ้าชอบดองกด 3 ถ้าไม่กด 4")
+    console.log(s)
+    console.log(b)
+    if(s==3)
+    {
+    document.getElementById("naa").innerHTML ="งอนๆๆๆหนักมากจนผมเป็นลอน ถึงแม้ว่าจะไม่ค่อยมีผมก็ตามม T T"
+}
+else if(s==4)
+{
+    document.getElementById("naa").innerHTML ="D Jai Waa"
+}
+else
+{
+    document.getElementById("naa").innerHTML ="..... ตอบไม่ตรงขำถาม เหมือนมะขามไม่ปอกเปือก wtf"
+}
+}
+console.log(b)
+function n2()
+{
+if(b==2)
+{
+if(s==4)
+{
+    document.getElementById ("lmaon").innerHTML =alert("my fav music >////<")
+    document.getElementById ("music").innerHTML ="<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/SDXYlyUlkC0\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>"     +    "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/SctrVF37GAQ\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>"
+   
+}
+}
+
+else
+{
+    
+    alert("ไม่มีไรจริงดิ!!!!")
+}
+}
+
+
+
+
